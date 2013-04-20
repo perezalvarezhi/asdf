5f081f9f089139480f3b0804dd6b0798d168636e
Hugo Perez <perezalvarezhi@gmail.com>
2013-04-19 23:21:47 -0500

    newChanges

--- src/com/example/asdf/MainActivity.java
+++ src/com/example/asdf/MainActivity.java
@@ -3,13 +3,21 @@ package com.example.asdf;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.net.Uri;
 
 public class MainActivity extends Activity {
 
+	Button button;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		addListenerOnButton();
 	}
 
 	@Override
@@ -17,6 +25,25 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	
+	public void addListenerOnButton() {
+		 
+		button = (Button) findViewById(R.id.button1);
+ 
+		button.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View arg0) {
+ 
+			  Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.mkyong.com"));
+			    startActivity(browserIntent);
+ 
+			}
+ 
+		});
+ 
 	}
 
 }
